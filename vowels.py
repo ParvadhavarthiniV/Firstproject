88acc8727611489d0d86f6860335d7c7633555d7
ParvadhavarthiniV <39819745+ParvadhavarthiniV@users.noreply.github.com>
2018-07-12 14:45:37 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,20 +1,21 @@
 val=input()
-list1=['a','e','i','o','u','A','E','I','O','U']
+list1=['a','e','i','o','u']
 for i in list1:
-    if((val<='A' and val>='Z') or (val>='a' and val<='z')):
+    if(val>='a' and val<='z'):
         if(val==i):
             m=0
             break
         else:
-           m=1
-           break
+            m=1
+            break
     else:
         m=2
         break
 if(m==0):
-    print("Vowel")
+   print("Vowel")
 elif(m==1):
-    print("Consonant")
+   print("Consonant")
 else:
-    print("Invalid")
+   print("Invalid")
+        
         
